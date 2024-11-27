42fa0310253af588517e10da9c6a346a7e8af2c4
Rodney St Cloud <hoyrod1@aol.com>
2024-11-27 18:22:12 -0500

    Created a simple html, css and javascript page for the Web Essentials Class

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 console.log(`============== Sanity Check For Index.js ==============`);
+// const clickMe = document.getElementById("clickMe");
+// console.log(clickMe);
+// clickMe.addEventListener("click", testButton);
+// const testDiv = document.getElementsByClassName("start-button-div");
+// console.log(testDiv);
+
+// testDiv.addEventListener("click", testButton);
+const clickMe = document.querySelector("#div-button");
+// console.log(clickMe);
+clickMe.addEventListener("click", testButton);
+function testButton(e) {
+  alert("You have hit your target");
+}
